a285034bd5db05423037dec14e9d64e0a7623e57
Tamaarine <irebootplaygt@gmail.com>
2022-12-31 13:28:13 -0500

    Added: initial commit for coding the to-do-app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<script>
+export default {
+    data() {
+        return {
+            count: 0,
+            
+        }
+    }
+}
+
+</script>
+
+<template>
+    <h1>This is the count {{ count }}</h1>
+    
+</template>
